a5410d18a0b929c13765fe05f2e54fd9bf4ce5d5
penkesiva <penkesiva@gmail.com>
2019-02-15 12:08:15 -0800

    remove unnecessary permissions and add crash-analytics
    Signed-off-by: penkesiva <penkesiva@gmail.com>

--- app/src/main/java/com/ee/testprep/LoginActivity.java
+++ app/src/main/java/com/ee/testprep/LoginActivity.java
@@ -15,7 +15,6 @@ import android.view.animation.Animation;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
