ba3c30e1750dfd400595f9a4c703c2de3e72d4d2
Siva Penke <siva.penke@samsung.com>
2019-02-15 19:08:39 -0800

    Fonts: mostly using work-san fonts

--- app/src/main/java/com/ee/testprep/font/FontUtil.java
+++ app/src/main/java/com/ee/testprep/font/FontUtil.java
@@ -33,49 +33,49 @@ public class FontUtil {
 
     public static Typeface getRobotoRegular(Context context) {
         if (robotoRegular == null) {
-            robotoRegular = Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Regular.ttf");
+            robotoRegular = Typeface.createFromAsset(context.getAssets(), "fonts/roboto_regular.ttf");
         }
         return robotoRegular;
     }
 
     public static Typeface getRobotoMedium(Context context) {
         if (robotoMedium == null) {
-            robotoMedium = Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Medium.ttf");
+            robotoMedium = Typeface.createFromAsset(context.getAssets(), "fonts/roboto_medium.ttf");
         }
         return robotoMedium;
     }
 
     public static Typeface getRobotoBold(Context context) {
         if (robotoBold == null) {
-            robotoBold = Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Bold.ttf");
+            robotoBold = Typeface.createFromAsset(context.getAssets(), "fonts/roboto_bold.ttf");
         }
         return robotoBold;
     }
 
     public static Typeface getRobotoItalic(Context context) {
         if (robotoItalic == null) {
-            robotoItalic = Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Italic.ttf");
+            robotoItalic = Typeface.createFromAsset(context.getAssets(), "fonts/roboto_italic.ttf");
         }
         return robotoItalic;
     }
 
     public static Typeface getRobotoBoldItalic(Context context) {
         if (robotoBoldItalic == null) {
-            robotoBoldItalic = Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-BoldItalic.ttf");
+            robotoBoldItalic = Typeface.createFromAsset(context.getAssets(), "fonts/roboto_bold_italic.ttf");
         }
         return robotoBoldItalic;
     }
 
     public static Typeface getRobotoThinItalic(Context context) {
         if (robotoThinItalic == null) {
-            robotoThinItalic = Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-ThinItalic.ttf");
+            robotoThinItalic = Typeface.createFromAsset(context.getAssets(), "fonts/roboto_thin_italic.ttf");
         }
         return robotoThinItalic;
     }
 
     public static Typeface getSegoePrint(Context context) {
         if (segoePrint == null) {
-            segoePrint = Typeface.createFromAsset(context.getAssets(), "fonts/Segoe-Print.ttf");
+            segoePrint = Typeface.createFromAsset(context.getAssets(), "fonts/segoe_print.ttf");
         }
         return segoePrint;
     }
