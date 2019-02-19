c082a811c35c29c2ca4cca3163e6514c61679653
Srinivasa Algubelli <s.algubelli@gmail.com>
2019-02-18 17:43:51 -0800

    Fix for crash on pressing phone recent button being in quiz fragment

--- app/src/main/java/com/ee/testprep/db/Test.java
+++ app/src/main/java/com/ee/testprep/db/Test.java
@@ -1,6 +1,8 @@
 package com.ee.testprep.db;
 
 import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import com.ee.testprep.R;
 
@@ -12,7 +14,7 @@ import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "tests_data")
-public class Test {
+public class Test implements Parcelable {
     @NonNull
     @PrimaryKey
     @ColumnInfo(name = "test_name")
@@ -28,6 +30,15 @@ public class Test {
     @ColumnInfo(name = "time_used")
     public int timeUsed;
 
+    public Test(Parcel parcel) {
+        testName = parcel.readString();
+        totalCount = parcel.readInt();
+        answeredCount = parcel.readInt();
+        correctCount = parcel.readInt();
+        wrongCount = parcel.readInt();
+        timeUsed = parcel.readInt();
+    }
+
     public Test(String testName, int totalCount, int answeredCount, int correctCount,
             int wrongCount, int timeUsed) {
         this.testName = testName;
@@ -38,10 +49,37 @@ public class Test {
         this.timeUsed = timeUsed;
     }
 
+    public static final Creator<Test> CREATOR = new Creator<Test>() {
+        @Override
+        public Test createFromParcel(Parcel in) {
+            return new Test(in);
+        }
+
+        @Override
+        public Test[] newArray(int size) {
+            return new Test[size];
+        }
+    };
+
     @Override
     public String toString() {
         return testName + ": total - " + totalCount + ", answered - " + answeredCount + ", " +
                 "correct - " + correctCount + ", wrong - " + wrongCount + ", time used = " + timeUsed;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(testName);
+        dest.writeInt(totalCount);
+        dest.writeInt(answeredCount);
+        dest.writeInt(correctCount);
+        dest.writeInt(wrongCount);
+        dest.writeInt(timeUsed);
     }
 
     public enum TestType {
